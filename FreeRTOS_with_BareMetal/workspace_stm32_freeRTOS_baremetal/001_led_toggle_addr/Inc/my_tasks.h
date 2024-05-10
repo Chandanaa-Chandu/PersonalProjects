1b3129ce8f1476fa64e8ed96ca31c13089b49a5e
Chandanaa-Chandu <114139501+Chandanaa-Chandu@users.noreply.github.com>
2024-05-10 10:40:59 +0100

    Add files via upload

---
+++ FreeRTOS_with_BareMetal/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/Inc/my_tasks.h
@@ -0,0 +1,24 @@
+/*
+ * my_tasks.h
+ *
+ *  Created on: May 8, 2024
+ *      Author: chandanar
+ */
+
+#ifndef MY_TASKS_H_
+#define MY_TASKS_H_
+
+/* Includes necessary freeRTOS header files */
+#include "freeRTOS.h"
+#include "task.h"
+
+/* Function prototypes */
+void initTasks(void);
+
+void taskSetup(void);
+
+void timerTaskStart(void);
+
+void timerTask(void* pvParameters);
+
+#endif /* MY_TASKS_H_ */
