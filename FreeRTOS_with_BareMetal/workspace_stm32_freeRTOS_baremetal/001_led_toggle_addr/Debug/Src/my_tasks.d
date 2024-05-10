3f30b4e2ea64cd3b31f8d72dcc2ed32c55f967c7
Chandanaa-Chandu <114139501+Chandanaa-Chandu@users.noreply.github.com>
2024-05-10 10:37:09 +0100

    Add files via upload

---
+++ FreeRTOS_with_BareMetal/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/Debug/Src/my_tasks.d
@@ -0,0 +1,22 @@
+Src/my_tasks.o: ../Src/my_tasks.c ../Inc/my_tasks.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/freeRTOS.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/FreeRTOSConfig.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/portable.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/task.h \
+ C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/list.h \
+ ../Inc/main.h
+../Inc/my_tasks.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/freeRTOS.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/FreeRTOSConfig.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/projdefs.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/portable.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/task.h:
+C:/Users/chandanar/Desktop/STM32_BareMetal_FreeRTOS/workspace_stm32_freeRTOS_baremetal/001_led_toggle_addr/FreeRTOS/Source/include/list.h:
+../Inc/main.h:
